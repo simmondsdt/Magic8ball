47ce2126ad75088becff7f7ca8bab318e7084d66
Daniel Simmonds <simmondsdt@gmail.com>
2016-10-27 22:50:09 -0600

    Organized the array

--- 8ball.rb
+++ 8ball.rb
@@ -3,7 +3,28 @@ require 'pry'
 puts "What is your name?"
 @user = gets.strip
 puts "Welcome to the Magic 8 Ball, #@user"
-@ans = ['It is certain', 'It is decidedly so', 'Without a doubt', 'Yes definitely', 'You may rely on it', 'As I see it, yes', 'Most likely', 'Outlook good', 'Yes', 'Signs point to yes', 'Reply hazy try again', 'Ask again later', 'Better not tell you now', 'Cannot predict now', 'Concentrate and ask again', 'Dont count on it', 'My reply is no', 'My sources say no', 'Outlook not so good', 'Very doubtful']
+@ans = [
+  'It is certain',
+  'It is decidedly so',
+  'Without a doubt',
+  'Yes definitely',
+  'You may rely on it',
+  'As I see it, yes',
+  'Most likely',
+  'Outlook good',
+  'Yes',
+  'Signs point to yes',
+  'Reply hazy try again',
+  'Ask again later',
+  'Better not tell you now',
+  'Cannot predict now',
+  'Concentrate and ask again',
+  'Dont count on it',
+  'My reply is no',
+  'My sources say no',
+  'Outlook not so good',
+  'Very doubtful'
+]
 @fun_ans = []
 # @ans2 = []
 # @fun_ans2 = []
